16582826e52e81c47fdbd1f782b7e765e429c4b8
David Mareček <marecek@ufal.mff.cuni.cz>
2024-09-28 02:47:22 +0200

    shuffling prefixes

--- data/tokenize_and_filter_variants.py
+++ data/tokenize_and_filter_variants.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import torch
 import transformers
+import random
 
 #PROMPT_PREFIX = {"cs": "Jsem", "en": "I am a", "de": "Ich bin"}
 PROMPT_PREFIX = {"cs": ["Jsem", "Jsi", "Jste", "Budu", "Budeš", "Budete", "Pracuji jako", "Pracuješ jako", "Pracujete jako"],
@@ -44,6 +45,8 @@ professions.sort()
 
 output = []
 
+clusters = {}
+
 for prof in professions:
     
     a_an = "a"
@@ -73,6 +76,11 @@ for prof in professions:
         if len(common_prefix) < 3:
             print("Prefix too short", common_prefix)
         elif female_suffix != male_suffix:
+            comp = male_suffix + " " + female_suffix
+            if comp in clusters:
+                clusters[comp].append(common_prefix)
+            else:
+                clusters[comp] = [common_prefix]
             for variant in range(9):
                 item = {"prompt": PROMPT_PREFIX[language][variant] + " " + common_prefix, 
                         "completions": [male_suffix, female_suffix],
@@ -81,6 +89,41 @@ for prof in professions:
                         "subject": prof
                        }
                 output.append(item)
+# merge clusters containing only one prefix into 'singletons'
+singletons = []
+comps_to_delete = []
+for c in clusters:
+    if len(clusters[c]) == 1:
+        singletons.append(clusters[c][0])
+        comps_to_delete.append(c)
+clusters["singletons"] = singletons
+for c in comps_to_delete:
+    del clusters[c]
+# shuffle clusters #variants times
+for c in clusters:
+    s = clusters[c].copy()
+    shuffled = [''] * 9*len(clusters[c])
+    permutation_count = 0
+    limit = 100
+    while limit > 0 and permutation_count < 9:
+        random.shuffle(s)
+        for i in range(len(s)):
+            if s[i] == clusters[c][i]:
+                break
+            if i == len(s) - 1:
+                for j, x in enumerate(s):
+                    shuffled[(len(s)-j-1)*9 + permutation_count] = x
+                permutation_count += 1
+        limit -= 1
+    clusters[c] = shuffled
+
+for i in range(len(output)):
+    comp = output[i]["completions"][0] + " " + output[i]["completions"][1]
+    if comp in clusters.keys():
+        output[i]["shuffled_prefix"] = clusters[comp].pop()
+    else:
+        output[i]["shuffled_prefix"] = clusters["singletons"].pop()
+
 
 out_file = open(language+"_train_"+sys.argv[2]+".json", "w")
 json.dump(output, out_file, indent = 4, ensure_ascii=False)
